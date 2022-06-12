a8277327dbdf7019ecb53a2c93e52d2bb6082e92
Gabrielle Lima <gabrielle.clima23@gmail.com>
2022-06-12 14:03:08 -0300

    Aulas iniciais do curso

--- aula6/index.js
+++ aula6/index.js
@@ -1,7 +1,20 @@
-let nome = "Gabrielle"; // escopo de bloco
+/* let nome = "Alexandre"; // escopo de bloco
 
-console.log("Alexandre nasceu em 1984");
-console.log("Em 2019 Gabrielle conheceu Alexandre");
-console.log("Em 2021 Gabrielle casou com Alexandre");
-console.log("Gabrielle e Alexandre são pais do duke");
+console.log(nome, "nasceu em 1984");
+console.log("Em 2019 Gabrielle conheceu ", nome);
+console.log("Em 2021 Gabrielle casou com ", nome);
+console.log("Gabrielle e", nome, "são pais do duke");
 console.log("O filho deles se chama dukinho");
+ */
+
+let nome; // declaração da variável
+nome = "abcde"; // inicialização da variável
+console.log(nome);
+nome = "abcdefg";
+console.log(nome);
+// pode ser reatribuído mas nunca redeclarado
+// Variáveis precisam ter nomes significativos
+// Não usar palavras reservadas
+let nomeCliente = "João";
+console.log(nomeCliente);
+// Não podem iniciar com números
