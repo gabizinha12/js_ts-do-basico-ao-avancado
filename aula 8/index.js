282b0811184d3b29aa9f05b7ff1f2b721b7819d6
Gabrielle Lima <gabrielle.clima23@gmail.com>
2022-07-02 01:36:00 -0300

    fix exercício

--- aula 8/index.js
+++ aula 8/index.js
@@ -3,7 +3,7 @@ Luiz Otávio Miranda tem 30 anos, pesa 84 kg
 tem 1.8 de altura e seu IMC é de 25.925925925925924
 */
 const nome = "Gabrielle Cristine";
-const sobreNome = "de Lima Pereira";
+const sobrenome = "de Lima Pereira";
 const peso = 58;
 const idade = 21;
 const altura = 1.5;
@@ -11,6 +11,6 @@ let IMC = peso / (altura * altura); // peso / (altura * altura)
 let anoNascimento = 2022 - idade;
 console.log(anoNascimento);
 
-/* console.log(
-  `${nome} ${sobreNome} tem 30 anos, pesa ${peso} kg tem ${alturaEmM} de altura e seu IMC é de ${IMC}`
-); */
+console.log(`${nome} ${sobrenome} tem ${idade} anos, pesa ${peso} kg`);
+console.log(`tem ${alturaEmM} de altura e seu IMC é de ${IMC}`);
+console.log(`${nome} nasceu em ${anoNascimento}`);
